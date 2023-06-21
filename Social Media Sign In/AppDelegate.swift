0a08de0e64e28a19c35c78673c2f2d3207523e49
Hitesh Takhtani <hitesht.itpath@gmail.com>
2023-06-21 18:18:01 +0530

    Apple and Google login Completed

--- Social Media Sign In/AppDelegate.swift
+++ Social Media Sign In/AppDelegate.swift
@@ -6,38 +6,59 @@
 //
 
 import UIKit
-import FirebaseCore
 import GoogleSignIn
 
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        FirebaseApp.configure()
-        
-        
-        GIDSignIn.sharedInstance.restorePreviousSignIn()
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+            if error != nil || user == nil {
+                // Show the app's signed-out state.
+            } else {
+                // Show the app's signed-in state.
+            }
+        }
         return true
     }
-    
-    func application(_ app: UIApplication,open url: URL,options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        return GIDSignIn.sharedInstance.handle(url)
+
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
+        var handled = false
+        
+        handled = GIDSignIn.sharedInstance.handle(url)
+        
+//        if !handled {
+//            handled = ApplicationDelegate.shared.application(
+//                app,
+//                open: url,
+//                sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+//                annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+//            )
+//        }
+        
+        return handled
     }
     
-    // MARK: UISceneSession Lifecycle
-    
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-    
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        
+
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {
+        // Handle discarded scene sessions
     }
 }
-
--- Social Media Sign In/ViewController.swift
+++ Social Media Sign In/ViewController.swift
@@ -5,48 +5,150 @@
 //  Created by IPS-108 on 19/06/23.
 //
 
+//FBSDKLoginButton
+// MARK: - Google Client ID
+///    1017714174199-g678psab576v9eprlpmpveq0gk1mgf21.apps.googleusercontent.com
+
+import AuthenticationServices
 import UIKit
 import GoogleSignIn
-import GoogleSignInSwift
-import FirebaseAuth
+
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var btnGoogle: UIButton!
-    @IBOutlet weak var btnApple: UIButton!
-    @IBOutlet weak var btnFacebook: UIButton!
-    @IBOutlet weak var btnTwitter: UIButton!
-    @IBOutlet weak var btngitHub: UIButton!
-    
+    let appleSignInBtn = ASAuthorizationAppleIDButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(appleSignInBtn)
+        appleSignInBtn.addTarget(self, action: #selector(appleSignIn), for: .touchUpInside)
     }
     
-    func btnClick(){
-        print("Hello")
-    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
-    @IBAction func google(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func apple(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func facebook(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func twitter(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func gitHub(_ sender: UIButton) {
+        let buttonWidth: CGFloat = 300
+        let buttonHeight: CGFloat = 50
+        let buttonX: CGFloat = (view.bounds.width - buttonWidth) / 2
+        let buttonY: CGFloat = 200 // Adjust this value to set the desired distance from the top
+
+        appleSignInBtn.frame = CGRect(x: buttonX, y: buttonY, width: buttonWidth, height: buttonHeight)
         
     }
 
+    
+    @objc func appleSignIn(){
+        let provider = ASAuthorizationAppleIDProvider()
+        let requset = provider.createRequest()
+        requset.requestedScopes = [.fullName,.email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [requset])
+        
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        
+        controller.performRequests()
+    }
+    
+    @IBAction func googleBtn(_ sender: UIButton) {
+        //GIDSignIn.sharedInstance.signOut()
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+            guard error == nil else { return }
+
+            print("Google login Sucessful")
+//            GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+//                guard error == nil else { return }
+//                guard let signInResult = signInResult else { return }
+//
+//                let user = signInResult.user
+//
+//                let emailAddress = user.profile?.email
+//
+//                let fullName = user.profile?.name
+//                let givenName = user.profile?.givenName
+//                let familyName = user.profile?.familyName
+//
+//                let profilePicUrl = user.profile?.imageURL(withDimension: 320)
+//
+//                print(emailAddress)
+//                print(fullName)
+//                print(profilePicUrl)
+//            }
+          }
+        
+    }
+    //MARK: - Spare Google SignIn
+    
+//    @IBAction func googleBtn(_ sender: UIButton) {
+//        GIDSignIn.sharedInstance.signOut() // Optional: You can sign out the user before initiating the sign-in process
+//
+//        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+//            if let error = error {
+//                print("Google sign-in error: \(error.localizedDescription)")
+//                return
+//            }
+//
+//            guard let signInResult = signInResult else {
+//                print("Google sign-in result is nil")
+//                return
+//            }
+//
+//            let user = signInResult.user
+//
+//            let emailAddress = user.profile!.email
+//            let fullName = user.profile!.name
+//            let givenName = user.profile?.givenName
+//            let familyName = user.profile?.familyName
+//
+//            let profilePicUrl = user.profile!.imageURL(withDimension: 320)
+//
+//            print("Email Address: \(emailAddress ?? "")")
+//            print("Full Name: \(fullName ?? "")")
+//            print("Profile Picture URL: \(profilePicUrl?.absoluteString ?? "")")
+//        }
+//    }
+    
+    //MARK: - Google SignOut
+    
+    //@IBAction func signOut(sender: Any) {
+      //GIDSignIn.sharedInstance.signOut()
+    //}
+    
+    
+    @IBAction func facebookBtn(_ sender: UIButton) {
+        
+        
+    }
 }
 
+extension ViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("Failed to Login!!")
+    }
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential{
+        case let credentials as ASAuthorizationAppleIDCredential:
+            let firstName = credentials.fullName?.givenName
+            let lastName = credentials.fullName?.familyName
+            let email = credentials.email
+            
+            print(firstName)
+            print(lastName)
+            print(email)
+            
+            break
+        default:
+            break
+        }
+        
+    }
+}
+
+extension ViewController: ASAuthorizationControllerPresentationContextProviding{
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
+    }
+    
+    
+}
